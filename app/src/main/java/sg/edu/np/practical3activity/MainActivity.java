268c09b7f2ec32faae0b7592781ca1ea72cf015e
Chea Hong Li <s10206182@connect.np.edu.sg>
2021-05-07 09:45:26 +0800

    Completed Practical 3

--- app/src/main/java/sg/edu/np/practical3activity/MainActivity.java
+++ app/src/main/java/sg/edu/np/practical3activity/MainActivity.java
@@ -2,11 +2,13 @@ package sg.edu.np.practical3activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "Create Activity";
@@ -23,22 +25,25 @@ public class MainActivity extends AppCompatActivity {
         Button followed = findViewById(R.id.button);
         Button msg = findViewById(R.id.button2);
 
+        Intent added = getIntent();
+        String add = added.getStringExtra("addInt");
+        name.setText(name.getText() + " " + add);
         followed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean follow = !user.isFollowed();
                 if (follow){
                     followed.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", '0').show();
                 }
                 else{
                     followed.setText("Follow");
+                    Toast.makeText(getApplicationContext(), "Unfollowed", '0').show();
                 }
                 user.setFollowed(follow);
 
                 System.out.println(follow);
             }
         });
-
-
     }
 }
